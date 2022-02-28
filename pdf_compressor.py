67d90de05e9508b347a5a6f5150f6e3fd3b27155
darwinlibioulle <darwinlibioulle@penguin>
2022-02-28 13:56:43 +0100

    requirements

---
+++ pdf_compressor.py
@@ -0,0 +1,18 @@
+import PyPDF2
+
+FILE_Path = ""
+
+# Compress an pdf file
+def compress_file(pdf_path):
+    """Image Compressor
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    
+    """
+
+if __name__ == "__main__":
+    compress_file(FILE_PATH)
